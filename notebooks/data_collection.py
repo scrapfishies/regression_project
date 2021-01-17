0a3e363e1d73c7ea713ff50527e1d33e63853d5c
Aaron <abarela13@gmail.com>
2021-01-16 21:58:57 -0800

    Completed Scraper

--- notebooks/data_collection.py
+++ notebooks/data_collection.py
@@ -1,16 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
+import requests
 import pickle
-import os.path
-from os import path
-import math
 import time
+from bs4 import BeautifulSoup
+from os import path
 
-def movies_by_release_year(year):
-	result = pd.DataFrame()
+def all_movies_for_year(year):
+	# initialize primary list
+	movie_list = []
 
-	# retrieve site data
+	# retrieve data for movies by year
 	data = requests.get(f'https://www.boxofficemojo.com/year/{year}')
 
 	# load data into bs4
@@ -23,19 +22,25 @@ def movies_by_release_year(year):
 		link = row.find_all('td')[1].find('a').get('href').split('?')[0][9:-1]
 		release_date = row.find_all('td')[8].text
 		distributor = row.find_all('td')[9].text[:-2]
-		result = result.append({'link': link, 'title': title, 'release_date': release_date, 'distributor': distributor}, ignore_index=True)	
+		movie_list.append([link, title, release_date, distributor])
 
-	return result
+	return pd.DataFrame(movie_list, columns=['link', 'title', 'release_date', 'distributor'])
+
+def generate_yearly_movies_index(start_year, end_year):
+	for year in range(end_year, start_year - 1, -1):
+		if path.exists(f'../data/yearly_index/{year}_index'):
+			continue
+		else:
+			print(f'creating {year}_index')
+			yearly_movies_index = all_movies_for_year(year)
+			pickle.dump(yearly_movies_index, open(f'../data/yearly_index/{year}_index', 'wb'))
 
 def retrieve_movie_information(link, title, release_date, distributor):
-	result = {}
-
 	# retrieve site data
 	data = requests.get(f'https://www.boxofficemojo.com/release/{link}')
 
 	# load data into bs4
 	soup = BeautifulSoup(data.text, features="lxml")
-
 
 	domestic_gross = soup.find('div', { 'class': 'a-section a-spacing-none mojo-performance-summary-table' }).find('span', { 'class': 'money' }).text.replace(',','')[1:]
 
@@ -64,90 +69,52 @@ def retrieve_movie_information(link, title, release_date, distributor):
 		release_duration = release_duration.parent.findNext('span').text.split(' ')[0]
 	else:
 		release_duration = None
-		
-	result['title'] = title
-	result['domestic_gross'] = domestic_gross
-	result['distributor'] = distributor
-	result['budget'] = budget
-	result['release_date'] = release_date
-	result['rating'] = rating
-	result['genre'] = genre
-	result['release_duration'] = release_duration
 
-	# print(f'appending {result}')
-	return result
+	return [title,domestic_gross,distributor,budget,release_date,rating,genre,release_duration]
 
+def generate_movies_2d_list(yearly_movies_list_df):
+	year_list = []
 
-def create_yearly_movies_list_dataframes(start_year, end_year):
+	for idx, row in yearly_movies_list_df.iterrows():
+		if (idx > 0) & (idx % 15 == 0):
+			print(f'Pausing at idx {idx}')
+			time.sleep(3)
+			print('Resuming')
+		year_list.append(retrieve_movie_information(row.link, row.title, row.release_date, row.distributor))
+	
+	return year_list
+
+def generate_yearly_lists(start_year, end_year):
 	for year in range(end_year, start_year - 1, -1):
-		yearly_movies_list_df = pd.DataFrame()
-
-		if path.exists(f'../data/movies_list/{year}_movies_list_df'):
-			print(f'{year}_movies_list_df exists')
+		if path.exists(f'../data/yearly_scraped_movie_info/{year}_df'):
+			continue
 		else:
-			print(f'creating {year}_movies_list_df')
-			yearly_movies_list_df = movies_by_release_year(year)
-			pickle.dump(yearly_movies_list_df, open(f'../data/movies_list/{year}_movies_list_df', 'wb'))
+			if path.exists(f'../data/yearly_2d_lists/{year}_list'):
+				continue
+			else:
+				with open(f'../data/yearly_index/{year}_index','rb') as read_file:
+					year_index = pickle.load(read_file)
+				
+				print(f'creating 2d array - {year}')
 
+				year_list = generate_movies_2d_list(year_index)
+				
+				pickle.dump(year_list, open(f'../data/yearly_2d_lists/{year}_list', 'wb'))
 
-def chunk_creation(yearly_movies_list_df, year, delay, chunks, chunk_size):
-	columns=['title','domestic_gross', 'distributor', 'budget', 'release_date', 'rating', 'genre', 'release_duration']
+def generate_yearly_dataframes(start_year, end_year):
+	columns = ['title','domestic_gross', 'distributor', 'budget', 'release_date', 'rating', 'genre', 'release_duration']
 
-	for number in range(chunks):
-		if path.exists(f'../data/df_chunks/{year}_df_chunk_{number}'):
-			print(f'{year}_df_chunk_{number} found')
-		else:
-			print(f'creating {year}_df_chunk_{number}')
+	generate_yearly_movies_index(start_year, end_year)
 
-			chunk_df = pd.DataFrame(columns=columns)
-
-			start_pos = number * chunk_size
-			end_pos = (number + 1) * chunk_size if number < 19 else None
-
-			for idx, row in yearly_movies_list_df[start_pos:end_pos].iterrows():
-				chunk_df = chunk_df.append(retrieve_movie_information(row.link, row.title, row.release_date, row.distributor), ignore_index=True)
-				# if (idx % 25 ==0) and (idx > 0):
-				# 	print(f'pausing at {idx}')
-				# 	time.sleep(20)
-			
-			pickle.dump(chunk_df, open(f'../data/df_chunks/{year}_df_chunk_{number}', 'wb'))
-
-			print(f'----- {year} chunk {number} complete -----')
-
-			time.sleep(delay)
-
-def create_yearly_dataframes(start_year, end_year):
-	create_yearly_movies_list_dataframes(start_year, end_year)
-
-	print(f'------------------')
-	print(f'creating chunks')
-	print(f'------------------')
+	generate_yearly_lists(start_year, end_year)
 
 	for year in range(end_year, start_year - 1, -1):
-		if path.exists(f'../data/yearly_df/{year}_df'):
-			print(f'{year}_df exists')
+		if path.exists(f'../data/yearly_scraped_movie_info/{year}_df'):
+			continue
 		else:
-			with open(f'../data/movies_list/{year}_movies_list_df','rb') as read_file:
-				yearly_movies_list_df = pickle.load(read_file)
-			
-			delay = 30
-			chunks = 20
-			chunk_size = math.floor(yearly_movies_list_df.title.count()/chunks)
-			chunk_creation(yearly_movies_list_df, year, delay, chunks, chunk_size)
+			with open(f'../data/yearly_2d_lists/{year}_list','rb') as read_file:
+				year_list = pickle.load(read_file)
 
-			print(f'\n')
-			print(f'------------------')
-			print(f'all chunks created')
-			print(f'------------------')
-			print(f'\n')
-			print(f'------------------')
-			print(f'creating {year}_df')
-			print(f'------------------')
+			df = pd.DataFrame(year_list, columns=columns)
 
-			# pickle.dump(result, open(f'../data/{year}_df', 'wb'))
-
-			# print(f'created {year}_df')
-
-		# print('Return merged dataframe here')
-
-	# return df
+			pickle.dump(df, open(f'../data/yearly_scraped_movie_info/{year}_df', 'wb'))
